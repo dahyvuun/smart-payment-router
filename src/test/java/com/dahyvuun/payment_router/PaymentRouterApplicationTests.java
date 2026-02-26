4030cc8b9c9e91ba212a7378595582ae4d5a63b1
dahyvuun <dahyvuun@gmail.com>
2026-02-26 15:12:50 +0100

    feat: initial project setup

---
+++ src/test/java/com/dahyvuun/payment_router/PaymentRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dahyvuun.payment_router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
